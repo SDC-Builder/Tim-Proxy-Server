fc67e754f4f375ae652697d4045134460bafaf4f
Tim Anderson <tdanderson805@gmail.com>
2021-06-09 22:17:08 -0700

    moving routes through proxy server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-require('newrelic');
+const axios = require('axios').default;
+// require('newrelic');
 
 const app = express();
 const PORT = 3000;
@@ -10,14 +10,35 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('/hi', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/about/bundle.js', async (req, res) => {
+  try {
+    const { data } = await axios.get('http://localhost:3002/bundle.js');
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/about/:id', async (req, res) => {
+  try {
+    const { data } = await axios.get(`http://localhost:3002/api/about/${req.params.id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`Proxy listening at port ${PORT}`);
 });
