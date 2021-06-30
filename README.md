aaf0504d601e7320840fed3cebf09a7e1bd15ff7
Tim Anderson <tdanderson805@gmail.com>
2021-06-29 22:03:40 -0700

    updating readme and removing misc comments

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
-# Proxy
+# Proxy Server and About Microservice
 
-Installation
-```sh
-$ npm install
-```
+- An about service and proxy server that has been scaled for production. 
+- Data is pulled from an About microservice via API requests, 10 Million records total.  
+- Server Side Rendering (SSR) allows fully rendered HTML pages to be delivered on initial load.  
+- HTML pages are cached in a Redis database.
+- A series of load balancers and horizontally scaled nodes accomodate 600 RPS to random routes.
+- Redis cache accomodates 1000 RPS for recently rendered pages.
 
-Start in development
-```sh
-$ npm run dev
-```
+## Deployment Architecture
+![Server Design (1)](https://user-images.githubusercontent.com/71040019/123899600-5eef8800-d91c-11eb-8650-daf6c353eedf.jpeg)
+
+## Related Projects
+
+  - https://github.com/SDC-Builder/Tim-About-Service
+  - https://github.com/SDC-Builder/Tim-Proxy-Server
+
+## Local Development
+
+- Run `npm install` from the root directory to install dependencies
+- Run `npm build` to build the webpack bundle
+- Run `npm start` to start the server. Server runs on port 3000
+
+
+
+## Deployment Performance
+
+Google PageSpeed insight score of 99
+
+<img width="660" alt="Screen Shot 2021-06-21 at 11 42 21 PM" src="https://user-images.githubusercontent.com/71040019/123899243-a6c1df80-d91b-11eb-8578-b66656dd44ca.png">
+
+
+600 RPS to random routes
+
+![Screen Shot 2021-06-16 at 9 41 07 PM](https://user-images.githubusercontent.com/71040019/123904626-a890a080-d925-11eb-9c21-933fc8737c2b.png)
+
+1000 RPS to previously rendered HTML routes
+
+![Screen Shot 2021-06-20 at 9 11 01 PM](https://user-images.githubusercontent.com/71040019/123904569-8a2aa500-d925-11eb-9d2d-10e38fb8be20.png)
+
+Screenshot of deployed page
+<img width="1792" alt="Screen Shot 2021-06-20 at 10 41 30 PM" src="https://user-images.githubusercontent.com/71040019/123904308-1ee0d300-d925-11eb-8f44-5adf781b4112.png">
--- server/express.js
+++ server/express.js
@@ -4,22 +4,20 @@ const axios = require('axios').default;
 const fs = require('fs');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const { promisify } = require("util");
+const { promisify } = require('util');
 
 const app = express();
 const PORT = 3000;
 
+// cache setup
 const redis = require('redis');
 
 const client = redis.createClient({
   host: '172.31.10.128',
   port: 6379,
 });
-
 const getCache = promisify(client.get).bind(client);
 const setCache = promisify(client.set).bind(client);
-
-// const client = redis.createClient();
 
 // import About component
 const { About } = require('../src/About');
@@ -34,14 +32,13 @@ app.get('/hi', (req, res) => {
   res.send('Hello World!');
 });
 
+// Loader.io test routes
 app.get('/loaderio-b5cce8ed78056fea633f38082af3c119', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'token.txt'));
 });
-
 app.get('/loaderio-0f0b671c71fa00f9eb121a3dd31083a5', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'token2.txt'));
 });
-
 app.get('/loaderio-1d93b97eba634c38967bbb88d2ac85cb', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'token3.txt'));
 });
@@ -65,52 +62,13 @@ app.get('/api/about/:id', async (req, res) => {
   }
 });
 
-// No SSR
-// app.get('/:id', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
-// });
-
-// const getData = async (id, cb) => {
-//   client.get(`${id}`, async (err, results) => {
-//     if (results) {
-//       cb(JSON.parse(results));
-//     } else {
-//       const { data } = await axios.get(`http://172.31.5.204:3002/api/about/${id}`);
-//       client.setex(`${id}`, 5, JSON.stringify(data));
-//       cb(data);
-//     }
-//   });
-// };
-
-// SSR
-// app.get('/:id', async (req, res) => {
-//   getData(req.params.id, (dbObj) => {
-//     let indexHTML = fs.readFileSync(path.resolve(__dirname, '../public', 'index.html'), {
-//       encoding: 'utf8',
-//     });
-
-//     // const css = new Set() // CSS for all rendered React components
-//     // const insertCss = (...styles) => styles.forEach(style => css.add(style._getCss()))
-//     let appHTML = ReactDOMServer.renderToString(
-//         <About state={dbObj}/>
-//     );
-    
-//     indexHTML = indexHTML.replace('<div id="about" class="spaced"></div>', `<div id="about" class="spaced"> ${appHTML} </div><script>window.__INITIAL__DATA__ = ${JSON.stringify(dbObj)}</script>`);
-
-//     res.contentType('text/html');
-//     res.status(200);
-//     return res.send(indexHTML);
-//   });
-// });
-
-// SSR Refactor
+// Server Side Rendering, check cache for previously rendered HTML
 app.get('/:id', async (req, res) => {
   const cachedHTML = await getCache(req.params.id);
   if (cachedHTML) { return res.send(cachedHTML); }
   let indexHTML = fs.readFileSync(path.resolve(__dirname, '../public', 'index.html'), {
     encoding: 'utf8',
   });
-  // const { data } = await axios.get(`http://localhost:3002/api/about/${req.params.id}`);
   const { data } = await axios.get(`http://172.31.5.204:3002/api/about/${req.params.id}`);
   const appHTML = ReactDOMServer.renderToString(<About state={data} />);
   indexHTML = indexHTML.replace('<div id="about" class="spaced"></div>', `<div id="about" class="spaced"> ${appHTML} </div><script>window.__INITIAL__DATA__ = ${JSON.stringify(data)}</script>`);
